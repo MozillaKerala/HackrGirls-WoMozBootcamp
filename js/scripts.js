138c8165a97f7b67e621f34efeb313dacdf09f35
shinenelson <radioactive.life@gmail.com>
2015-02-09 05:50:41 +0530

    Schedule and other minor changes

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function($) {
+	var $timeline_block = $('.cd-timeline-block');
+
+	//Hide timeline blocks which are outside the viewport
+	$timeline_block.each(function(){
+		if($(this).offset().top > $(window).scrollTop()+$(window).height()*0.75) {
+			$(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+		}
+	});
+
+	//On scoll, show/animate timeline blocks within the viewport
+	$(window).on('scroll', function(){
+		$timeline_block.each(function(){
+			if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
+				$(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+			}
+		});
+	});
+});
